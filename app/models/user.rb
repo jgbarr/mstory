823893e33c21d9abede523f22948674b15b89fb0
Jeff Barr <jeff@jeffbarr.com>
2011-04-14 14:29:19 -0700

    signin/signout

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,9 +34,14 @@ class User < ActiveRecord::Base
   end
 
   def self.authenticate(email, submitted_password)
-    user = find_by_emai(email)
-    return nil if user.nil?
+    user = find_by_email(email)
+    return nil  if user.nil?
     return user if user.has_password?(submitted_password)
+  end
+
+  def self.authenticate_with_salt(id, cookie_salt)
+    user = find_by_id(id)
+    (user && user.salt == cookie_salt) ? user : nil
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Mstory::Application.routes.draw do
-
   resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
 
   match '/signup',  :to => 'users#new'
+  match '/signin',  :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
 
   match '/about', :to => 'pages#about'
   match '/contact', :to => 'pages#contact'
