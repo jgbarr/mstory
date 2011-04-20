1dc5a580c2ea099cf3118b2d1bf1239513523d36
Jeff Barr <jeff@jeffbarr.com>
2011-04-20 11:45:06 -0700

    fix delete post action in mico controller

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
   before_filter :authenticate, :only => [:create, :destroy]
+  before_filter :authorized_user, :only => :destroy
 
 
   #stub for future dev of public streams
